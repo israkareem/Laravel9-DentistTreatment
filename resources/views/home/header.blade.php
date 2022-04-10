75d8d929cda5842d7ebdbe828e0f5d0b99099cdf
isko <kanikawahawa@gmail.com>
2022-04-10 11:43:36 +0200

    Added home view files

---
+++ resources/views/home/header.blade.php
@@ -0,0 +1,26 @@
+<head>
+    <title>DentaCare - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700" rel="stylesheet">
+
+    <link rel="stylesheet" href=="{{asset('assets')}}/cssopen-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href=="{{asset('assets')}}/cssanimate.css">
+
+    <link rel="stylesheet" href=="{{asset('assets')}}/cssowl.carousel.min.css">
+    <link rel="stylesheet" href=="{{asset('assets')}}/cssowl.theme.default.min.css">
+    <link rel="stylesheet" href=="{{asset('assets')}}/cssmagnific-popup.css">
+
+    <link rel="stylesheet" href=="{{asset('assets')}}/cssaos.css">
+
+    <link rel="stylesheet" href=="{{asset('assets')}}/cssionicons.min.css">
+
+    <link rel="stylesheet" href=="{{asset('assets')}}/cssbootstrap-datepicker.css">
+    <link rel="stylesheet" href=="{{asset('assets')}}/cssjquery.timepicker.css">
+
+
+    <link rel="stylesheet" href=="{{asset('assets')}}/cssflaticon.css">
+    <link rel="stylesheet" href=="{{asset('assets')}}/cssicomoon.css">
+    <link rel="stylesheet" href=="{{asset('assets')}}/cssstyle.css">
+</head>
---
+++ resources/views/home/sidebar.blade.php
@@ -0,0 +1,59 @@
+
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700" rel="stylesheet">
+
+<link rel="stylesheet" href="{{asset('assets')}}/css/open-iconic-bootstrap.min.css">
+<link rel="stylesheet" href="{{asset('assets')}}/css/animate.css">
+
+<link rel="stylesheet" href="{{asset('assets')}}/css/owl.carousel.min.css">
+<link rel="stylesheet" href="{{asset('assets')}}/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="{{asset('assets')}}/css/magnific-popup.css">
+
+<link rel="stylesheet" href="{{asset('assets')}}/css/aos.css">
+
+<link rel="stylesheet" href="{{asset('assets')}}/css/ionicons.min.css">
+
+<link rel="stylesheet" href="{{asset('assets')}}/css/bootstrap-datepicker.css">
+<link rel="stylesheet" href="{{asset('assets')}}/css/jquery.timepicker.css">
+
+
+<link rel="stylesheet" href="{{asset('assets')}}/css/flaticon.css">
+<link rel="stylesheet" href="{{asset('assets')}}/css/icomoon.css">
+<link rel="stylesheet" href="{{asset('assets')}}/css/style.css">
+
+
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">Denta<span>Care</span></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="oi oi-menu"></span> Menu
+            </button>
+
+            <div class="collapse navbar-collapse" id="ftco-nav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+                    <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
+                    <li class="nav-item"><a href="doctors.html" class="nav-link">Doctors</a></li>
+                    <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+                    <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+                    <li class="nav-item cta"><a href="contact.html" class="nav-link" data-toggle="modal" data-target="#modalRequest"><span>Make an Appointment</span></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- END nav -->
+
+    <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav ml-auto">
+
+            <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+            <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
+            <li class="nav-item"><a href="doctors.html" class="nav-link">Doctors</a></li>
+            <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+            <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+            <li class="nav-item cta"><a href="contact.html" class="nav-link" data-toggle="modal" data-target="#modalRequest"><span>Make an Appointment</span></a></li>
+        </ul>
+    </div>
+</div>
+
