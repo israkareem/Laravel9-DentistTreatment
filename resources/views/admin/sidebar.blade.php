6bdca89a6a56f8fcfb6fc187577757c5735688c6
isko <kanikawahawa@gmail.com>
2022-04-10 11:47:35 +0200

    added admin view files

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,1 +1,133 @@
-<?php
+<!-- MENU SIDEBAR-->
+<aside class="menu-sidebar d-none d-lg-block">
+    <div class="logo">
+        <a href="#">
+            <img src="{{asset('assets')}}/admin/images/icon/logo.png" alt="Cool Admin" />
+        </a>
+    </div>
+    <div class="menu-sidebar__content js-scrollbar1">
+        <nav class="navbar-sidebar">
+            <ul class="list-unstyled navbar__list">
+                <li class="active has-sub">
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                    <ul class="list-unstyled navbar__sub-list js-sub-list">
+                        <li>
+                            <a href="index.html">Dashboard 1</a>
+                        </li>
+                        <li>
+                            <a href="index2.html">Dashboard 2</a>
+                        </li>
+                        <li>
+                            <a href="index3.html">Dashboard 3</a>
+                        </li>
+                        <li>
+                            <a href="index4.html">Dashboard 4</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="chart.html">
+                        <i class="fas fa-chart-bar"></i>Charts</a>
+                </li>
+                <li>
+                    <a href="table.html">
+                        <i class="fas fa-table"></i>Tables</a>
+                </li>
+                <li>
+                    <a href="form.html">
+                        <i class="far fa-check-square"></i>Forms</a>
+                </li>
+                <li>
+                    <a href="calendar.html">
+                        <i class="fas fa-calendar-alt"></i>Calendar</a>
+                </li>
+                <li>
+                    <a href="map.html">
+                        <i class="fas fa-map-marker-alt"></i>Maps</a>
+                </li>
+                <li class="has-sub">
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-copy"></i>Pages</a>
+                    <ul class="list-unstyled navbar__sub-list js-sub-list">
+                        <li>
+                            <a href="login.html">Login</a>
+                        </li>
+                        <li>
+                            <a href="register.html">Register</a>
+                        </li>
+                        <li>
+                            <a href="forget-pass.html">Forget Password</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="has-sub">
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-desktop"></i>UI Elements</a>
+                    <ul class="list-unstyled navbar__sub-list js-sub-list">
+                        <li>
+                            <a href="button.html">Button</a>
+                        </li>
+                        <li>
+                            <a href="badge.html">Badges</a>
+                        </li>
+                        <li>
+                            <a href="tab.html">Tabs</a>
+                        </li>
+                        <li>
+                            <a href="card.html">Cards</a>
+                        </li>
+                        <li>
+                            <a href="alert.html">Alerts</a>
+                        </li>
+                        <li>
+                            <a href="progress-bar.html">Progress Bars</a>
+                        </li>
+                        <li>
+                            <a href="modal.html">Modals</a>
+                        </li>
+                        <li>
+                            <a href="switch.html">Switchs</a>
+                        </li>
+                        <li>
+                            <a href="grid.html">Grids</a>
+                        </li>
+                        <li>
+                            <a href="fontawesome.html">Fontawesome Icon</a>
+                        </li>
+                        <li>
+                            <a href="typo.html">Typography</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
+<!-- END MENU SIDEBAR-->
+
+
+<!-- Jquery JS-->
+<script src="{{asset('assets')}}/admin/vendor/jquery-3.2.1.min.js"></script>
+<!-- Bootstrap JS-->
+<script src="{{asset('assets')}}/admin/vendor/bootstrap-4.1/popper.min.js"></script>
+<script src="{{asset('assets')}}/admin/vendor/bootstrap-4.1/bootstrap.min.js"></script>
+<!-- Vendor JS       -->
+<script src="{{asset('assets')}}/admin/vendor/slick/slick.min.js">
+</script>
+<script src="{{asset('assets')}}/admin/vendor/wow/wow.min.js"></script>
+<script src="{{asset('assets')}}/admin/vendor/animsition/animsition.min.js"></script>
+<script src="{{asset('assets')}}/admin/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+</script>
+<script src="{{asset('assets')}}/admin/vendor/counter-up/jquery.waypoints.min.js"></script>
+<script src="{{asset('assets')}}/admin/vendor/counter-up/jquery.counterup.min.js">
+</script>
+<script src="{{asset('assets')}}/admin/vendor/circle-progress/circle-progress.min.js"></script>
+<script src="{{asset('assets')}}/admin/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="{{asset('assets')}}/admin/vendor/chartjs/Chart.bundle.min.js"></script>
+<script src="{{asset('assets')}}/admin/vendor/select2/select2.min.js">
+</script>
+
+<!-- Main JS-->
+<script src="{{asset('assets')}}/admin/js/main.js"></script>
+
